f95ea4216c573b28352e1f5e8841da6c2e969e8d
juan2ramos <juan2ramos@gmail.com>
2018-04-24 16:26:57 -0500

    ok diseno

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -76,7 +76,7 @@
 			margin: 20px auto 0;
 			font-size: 18px;
 		}
-		@media (max-width: 420px) {
+		@media (max-width: 600px) {
 			html, body {
 				background-image:linear-gradient(to right, rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)),  url("images/back.jpg");
 			}
